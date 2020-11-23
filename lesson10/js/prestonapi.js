ce9a835a62b322bdaeaf92405a756eb789e33ab9
bjneib <nei15004@byui.edu>
2020-11-23 11:16:04 -0700

    api update

--- lesson10/js/prestonapi.js
+++ lesson10/js/prestonapi.js
@@ -5,7 +5,7 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
     const description = jsObject.weather[0].description;
-    document.getElementById('current').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+    document.getElementById('current').innerHTML = description;
     document.getElementById('temp').innerHTML = Math.round(jsObject.main.temp);
     document.getElementById('humidity').innerHTML = jsObject.main.humidity;
     document.getElementById('windspeed').innerHTML = Math.round(jsObject.wind.speed);
