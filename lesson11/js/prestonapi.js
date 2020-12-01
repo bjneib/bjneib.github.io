6e69ee556bbb4b4941035cf560e56f4be1a4e9fb
bjneib <nei15004@byui.edu>
2020-11-30 23:59:34 -0700

    api update

--- lesson11/js/prestonapi.js
+++ lesson11/js/prestonapi.js
@@ -5,10 +5,10 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
     const description = jsObject.weather[0].description;
-    document.getElementById('current').innerHTML = description;
-    document.getElementById('temp').innerHTML = Math.round(jsObject.main.temp);
-    document.getElementById('humidity').innerHTML = jsObject.main.humidity;
-    document.getElementById('windspeed').innerHTML = Math.round(jsObject.wind.speed);
+    document.getElementById("current").innerHTML = description;
+    document.getElementById("temp").innerHTML = Math.round(jsObject.main.temp);
+    document.getElementById("humidity").innerHTML = jsObject.main.humidity;
+    document.getElementById("windspeed").innerHTML = Math.round(jsObject.wind.speed);
   });
 
 const apiURLForecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=5fd1f91d346d40de19dc065f7b7cf45f";
@@ -26,7 +26,7 @@ fetch(apiURLForecast)
 
     let day = 0;
     for (item of jsObject.list) {
-      if (item.dt_txt.includes('18:00:00')) {
+      if (item.dt_txt.includes("18:00:00")) {
         const date = new Date(item.dt * 1000).getDay();
         const iconsrc = "https://openweathermap.org/img/w/" + item.weather[0].icon + ".png";
         const temp = item.main.temp_max;
@@ -53,7 +53,7 @@ fetch(requestURL)
         for (let i = 0; i < data.length; i++) {
           const event = document.createElement("p");
           event.innerText = data[i];
-          document.querySelector('.events').appendChild(event);
+          document.querySelector(".events").appendChild(event);
         }
       }
     }
