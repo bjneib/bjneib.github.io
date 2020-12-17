d334873db09cfdfab1a1ad7a391dbfd8aa4dada3
bjneib <nei15004@byui.edu>
2020-12-16 22:00:40 -0700

    update

--- scoots/js/rentals.js
+++ scoots/js/rentals.js
@@ -1,1 +1,11 @@
-const url = "https://bjneib.github.io/rentals.json";
+const urlRequest = "https://bjneib.github.io/rentals.json";
+
+fetch(urlRequest)
+.then(function (response) {
+   return response.json();
+})
+.then(function (jsonObject) {
+   console.table(jsonObject);  // temporary checking for valid response and data parsing
+   const table = jsonObject['rentals'];
+  
+});
