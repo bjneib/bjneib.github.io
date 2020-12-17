3db8161567b47d1380863815296f9339aca82f3a
bjneib <nei15004@byui.edu>
2020-12-17 12:41:07 -0700

    json update

--- scoots/js/rentals.js
+++ scoots/js/rentals.js
@@ -1,11 +1,24 @@
 const urlRequest = "https://bjneib.github.io/rentals.json";
 
 fetch(urlRequest)
-.then(function (response) {
-   return response.json();
-})
-.then(function (jsonObject) {
-   console.table(jsonObject);  // temporary checking for valid response and data parsing
-   const table = jsonObject['rentals'];
-  
-});
+   .then(function (response) {
+      return response.json();
+   })
+   .then(function (jsonObject) {
+      console.table(jsonObject); // temporary checking for valid response and data parsing
+      const table = jsonObject['rentals'];
+
+      const persons = document.querySelectorAll(".persons");
+      const halfRes = document.querySelectorAll(".halfRes");
+      const fullRes = document.querySelectorAll(".fullRes");
+      const halfWalk = document.querySelectorAll(".halfWalk");
+      const fullWalk = document.querySelectorAll(".fullWalk");
+
+      let row = 0;
+
+      persons[row].querySelector(".persons");
+      temps[day].innerHTML = temp.toFixed(0);
+      imgs[day].setAttribute("src", iconsrc);
+
+      row++;
+   });
